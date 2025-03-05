227a81ab9700470e982d9824a8699cb5470dc9b8
Алексєєнко Єлизавета <lizaaleks2005@gmail.com>
2025-03-05 23:09:26 +0000

    BasicSettings

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello, World!</h1>
+        <p>Welcome to the H2OFlow team 4 React app.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
