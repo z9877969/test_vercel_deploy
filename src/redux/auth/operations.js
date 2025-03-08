e77f75ffc546a3d7d5ba4d163b9ae2e7204f86bf
Yelyzaveta Aleksieienko <lizaaleks2005@gmail.com>
2025-03-08 19:17:25 +0000

    Revert "Adv section"

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const userAPI = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://h2oflow-team4-backend.onrender.com",
   withCredentials: true,
 });
 export const setAuthHeader = (token) => {
