cefb73960d31dee88c3ba0e0002620116141929e
Maryna Liashchenko <127967599+Maryna-01@users.noreply.github.com>
2025-03-08 16:23:25 +0100

    abdate modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,26 +2,29 @@ import css from './Modal.module.css';
 import Modal from 'react-modal';
 
 
-const sprite = '/img/sprite.svg';
+const sprite = '/sprite.svg';
+
 
 Modal.setAppElement('#root'); 
 
 export const BaseModal = ({isOpen, onClose, children }) =>{
     return (
-        <ReactModal
+        <Modal
       className={css.modal}
       isOpen={isOpen}
       onRequestClose={onClose}
-      ariaHideApp={false}
+      shouldCloseOnOverlayClick={true}
+      shouldCloseOnEsc={true}
       overlayClassName={css.overlay}
+      
     >
     <button type="button" className={css.close} onClick={onClose}>
-    <svg className={css.svgClose}>
-          <use href={`${sprite}#`} />
-        </svg>
+       <svg className={css.svgClose}>
+          <use href={`${sprite}#icon-close`} />
+       </svg>
     </button>
      {children}
 
-    </ReactModal>
+    </Modal>
     );
 };
