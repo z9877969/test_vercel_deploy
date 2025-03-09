982d57e31780ba63fc733ea183c40cf9f2394078
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-03-09 13:25:19 +0200

    ready mobil version

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
 // import WaterDetailedInfo from "../../components/WaterDetailedInfo";
+import s from "./TrackerPage.module.css";
 
 const TrackerPage = () => {
   return (
-    <div>
+    <div className={s.wrapperPage}>
       <WaterMainInfo />
       {/* <WaterDetailedInfo /> */}
     </div>
