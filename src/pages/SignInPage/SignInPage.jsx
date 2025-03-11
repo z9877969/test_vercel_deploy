62060e7f064348713be2cd5002257cf7aef385c8
YD <yuriidiachuk@icloud.com>
2025-03-11 11:39:45 +0200

    add SignIn css for desktop

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import s from "./SignInPage.module.css";
 import Logo from "../../components/Logo/Logo.jsx";
 import SignInForm from "../../components/SignInForm/SignInForm.jsx";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection.jsx";
 const SignInPage = () => {
   return (
-    <div className={s.box}>
-      <Logo />
-      <SignInForm />
+    <div className={s.container}>
+      <div className={s.box}>
+        <Logo />
+        <SignInForm />
+      </div>
+      <div className={s.advantagesSection}>
+        <AdvantagesSection />
+      </div>
     </div>
   );
 };
