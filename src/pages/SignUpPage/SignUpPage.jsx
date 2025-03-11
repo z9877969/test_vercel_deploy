a21176a7d19593c1e7554d8c01f487a17c06dba0
YD <yuriidiachuk@icloud.com>
2025-03-11 11:39:02 +0200

    add css in SignIn and SignUp pages for desktop

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Logo from "../../components/Logo/Logo.jsx";
 import SignUpForm from "../../components/SignUpForm/SignUpForm.jsx";
 import s from "./SignUpPage.module.css";
+import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection.jsx";
 
 const SignUpPage = () => {
   return (
-    <div className={s.box}>
-      <Logo />
-      <SignUpForm />
+    <div className={s.container}>
+      <div className={s.box}>
+        <Logo />
+        <SignUpForm />
+      </div>
+      <div className={s.advantagesSection}>
+        <AdvantagesSection />
+      </div>
     </div>
   );
 };
