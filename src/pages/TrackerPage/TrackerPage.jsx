391c37210579845c2866d630ac6867dbe5a36f4c
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-03-11 18:27:03 +0200

    delete UsserSettingsModal component from page

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
 import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo";
 import s from "./TrackerPage.module.css";
-import UserSettingsModal from "../../components/UserSettingsModal/UserSettingsModal.jsx";
 
 const TrackerPage = () => {
   return (
     <div className={s.trackerPageWrapper}>
-      <UserSettingsModal />
       <WaterMainInfo />
       <WaterDetailedInfo />
     </div>
