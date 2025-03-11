1ab33c518dd3b106b57add543273119f225af316
Svitlana Mykhailiuk <svetiklan@gmail.com>
2025-03-11 20:02:52 +0200

    calendar-grid

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,3 +1,13 @@
-const MonthInfo = () => {};
+import Calendar from "../Calendar/Calendar";
+import css from "./MonthInfo.module.css";
+
+const MonthInfo = () => {
+  return (
+    <div>
+      <h2 className={css.header}>Month</h2>
+      <Calendar />
+    </div>
+  );
+};
 
 export default MonthInfo;
