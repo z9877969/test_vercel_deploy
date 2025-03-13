3cb793672088156e82933c59d7d60fc7e928473d
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-03-13 17:13:54 +0200

    ready modal desing

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import WaterMainInfo from "../../components/WaterMainInfo/WaterMainInfo";
 import WaterDetailedInfo from "../../components/WaterDetailedInfo/WaterDetailedInfo";
 import s from "./TrackerPage.module.css";
+import WaterModal from "../../components/WaterModal/WaterModal.jsx";
 
 const TrackerPage = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [operationType, setOperationType] = useState("add");
+
+  const openModal = (type) => {
+    setOperationType(type);
+    setModalOpen(true);
+  };
+
   return (
     <div className={s.trackerPageWrapper}>
       <WaterMainInfo />
       <WaterDetailedInfo />
+      <WaterModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        operationType={operationType}
+      />
     </div>
   );
 };
