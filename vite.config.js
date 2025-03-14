3caa9768ac49a87838f0bc853f14c94bcf7eb341
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2025-03-14 20:09:45 +0200

    fix vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-    // rollupOptions: {
-    //   external: ["yup"],
-    // },
+    rollupOptions: {
+      output: {
+        globals: {
+          yup: "yup",
+        },
+      },
+    },
   },
 });
