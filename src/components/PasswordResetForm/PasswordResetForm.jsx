44ec114aa59cce42f5f9e6b0f0f1dd589f719133
YD <yuriidiachuk@icloud.com>
2025-03-14 10:50:02 +0200

    add resetForm

--- src/components/PasswordResetForm/PasswordResetForm.jsx
+++ src/components/PasswordResetForm/PasswordResetForm.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { resetPasswordValidationSchema } from "../../validationSchemas/authValidation.js";
 
 const PasswordResetForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleClickShowRepeatPassword = () => {
+    setShowRepeatPassword(!showRepeatPassword);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(resetPasswordValidationSchema),
+    mode: "onChange",
+    defaultValues: { password: "", repeatPassword: "" },
+  });
   return <div></div>;
 };
 
